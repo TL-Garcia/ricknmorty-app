bb4ddeb50546a8044f8d11c9a9049a4ecf510650
tl-garcia <tomas.bl.garcia@gmail.com>
2024-05-04 12:54:45 +0200

    chore: Clean up Link & Navbar components

--- src/ui/sections/layout/components/nav-bar/NavBar.tsx
+++ src/ui/sections/layout/components/nav-bar/NavBar.tsx
@@ -4,8 +4,12 @@ import './NavBar.scss';
 export const NavBar = () => {
   return (
     <nav className="navbar">
-      <NavLink href="/locations">Locations</NavLink>
-      <NavLink href="/episodes">Episodes</NavLink>
+      <NavLink href="/locations">
+        <h2>Locations</h2>
+      </NavLink>
+      <NavLink href="/episodes">
+        <h2>Episodes</h2>
+      </NavLink>
     </nav>
   );
 };
